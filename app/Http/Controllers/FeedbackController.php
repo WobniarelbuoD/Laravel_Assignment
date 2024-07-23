66f3842377a8b48e07723f8ee77a967e3b3ba051
WobniarelbuoD <WobniarelbuoD@gmail.com>
2024-07-23 11:51:53 +0300

    added working feedback request to database.

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -4,21 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Feedback;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class FeedbackController extends Controller
 {
-    function addFeedback(){
-        $feedback= new Feedback();
-        $feedback->game_id=Request('game_id');
-        $feedback->platform=Request('platform');
-        $feedback->version=Request('version');
-        $feedback->category=Request('category');
-        $feedback->content=Request('content');
-        dd($feedback->save());
-        if($feedback){
-            return ["result"=>$feedback];
+    function addFeedback(Request $req){
+        $rules=array(
+            "game_id"=>"required|max:11|",
+            "platform"=>"required|max:45|",
+            "version"=>"required|max:45|",
+            "category"=>"required|max:45|",
+            "content"=>"required|min:2|max:255|",
+        );
+        $validator= Validator::make($req->all(), $rules);
+        if($validator->fails()){
+            return $validator->errors();
+        }else if(DB::table('games')->where('id', $req->game_id)->exists()){
+            $feedback= new Feedback();
+            $feedback->game_id=$req->game_id;
+            $feedback->platform=$req->platform;
+            $feedback->version=$req->version;
+            $feedback->category=$req->category;
+            $feedback->content=$req->content;
+            $feedback->save();
+            if($feedback){
+                return ["result"=>$feedback];
+            }else{
+                return ["result"=>"Failure!"];
+            }
         }else{
-            return ["result"=>"Failure!"];
+            return ["result"=>"Game not found in the database."];
         }
+        
     }
 }
--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
+    public $timestamps = false;
     protected $fillable = [
         'game_id',
         'platform',
